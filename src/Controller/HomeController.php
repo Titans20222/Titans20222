9ceac06b56bfa532aa257399836e228f2dfd756b
Brahime El Hamed <elhamed.brahime@esprit.tn>
2022-02-14 19:52:09 +0100

    A1

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,7 +14,26 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'base.html.twig',
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    /**
+* @Route("/admin", name="admin")
+*/
+    public function admin(): Response
+    {
+        return $this->render('admin.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+
+    /**
+     * @Route("/admin1", name="admin1")
+     */
+    public function admin1(): Response
+    {
+        return $this->render('adminT.html.twig', [
+            'controller_name' => 'HomeController',
         ]);
     }
 }
